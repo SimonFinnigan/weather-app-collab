de59137228bf3b04da89b5745c6f015e5d12907a
SimonFinnigan <sifinnigan1@gmail.com>
2023-05-20 17:25:28 +0100

    server.js and scripts.js file now connected

--- scripts.js
+++ scripts.js
@@ -1,70 +1,101 @@
-function getWeather() {
-    const location = document.getElementById("location").value;
-    const apiKey = "f97f1ab753599aee7f08d7f884a89c89";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
-
-
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        const { main, weather } = data;
-        const { temp, feels_like, humidity } = main;
-        const { icon } = weather[0];
-        let degreeCelcius = (temp - 273)
-        let newTemp = parseFloat(degreeCelcius).toFixed(0);
-        const jsonLocation = data.name
-        const description = data.weather[0].main
-        const country  = data.sys.country
-        const weatherDiv = document.getElementById("weather");
-        weatherDiv.innerHTML = `
-        <div class="card">
-          <div class="card-header">
-          <div class="topRow">
-          <div id="icon"><img id="wicon" src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather icon"></div>
-          <p class="mb1 temperatureClass">${(newTemp)}°C</p>
-          <p class="mb1">${jsonLocation}, ${country}</h2>
-          </div>
-          <p class="mb1">${description}</p>
-          <p class="mb1">Humidity: ${humidity}%</p>
-          </div>
-          </div>
-          `;
-          $('#weather').removeClass("cloudyWeatherResults snowWeatherResults clearWeatherResults")
-          console.log(weather[0].id)
-          console.log(data)
-          $('#weather').each(function() {
-            if (weather[0].id > 800 && weather[0].id < 805) {
-              $(this).addClass('cloudyWeatherResults');
-              console.log("EQUALS")
-            } else if (weather[0].id > 199 && weather[0].id < 233) {
-              $(this).addClass('THUNDERSTORM');
-            } else if (weather[0].id > 299 && weather[0].id < 322) {
-              $(this).addClass('DRIZZLE');
-            } else if (weather[0].id > 499 && weather[0].id < 523) {
-              $(this).addClass('RAIN');
-            } else if (weather[0].id > 599 && weather[0].id < 623) {
-              $(this).addClass('snowWeatherResults');
-            } else if (weather[0].id > 700 && weather[0].id < 781) {
-              $(this).addClass('MIST/SMOKE/HAZE');
-            } else if (weather[0].id === 800) {
-              $(this).addClass('clearWeatherResults');
-              console.log(weather[0].id);
-            }
-        })
-        .catch(error => {
-          console.error('Error fetching weather data:', error);
-          const weatherDiv = document.getElementById("weather");
-          weatherDiv.innerHTML = `Could not fetch weather data. Please try again later.`;
-        });
-      }) 
-}
-  document.addEventListener('DOMContentLoaded', function () {
-    const intputField = document.getElementById("location");
-    var input = document.getElementById("location");
-    input.addEventListener("keypress", function(event) {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("searchButton").click();
+function fetchApiData(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(`Error fetching data: ${error}`);
     });
+}
+
+function getWeatherData(location) {
+  const url = `http://localhost:3000/weather/${location}`;
+  fetchApiData(url)
+    .then(data => {
+      if (data) {
+        displayWeatherData(data);
+        styleWeatherCard(data.weather[0].id);
+      }
+    })
+    .catch(() => {
+      const weatherDiv = document.getElementById("weather");
+      weatherDiv.innerHTML = `Could not fetch weather data. Please try again later.`;
+    });
+}
+
+function getForecastData(location) {
+  // const url = `http://localhost:3000/forecast/${location}`;
+  // fetchApiData(url)
+  //   .then(data => {
+  //     if (data) {
+  //       // Display the forecast data
+  //     }
+  //   })
+  //   .catch(() => {
+  //     // Handle error
+  //   });
+}
+
+function displayWeatherData(data) {
+  const { main, weather, name, sys } = data;
+  const { temp, humidity } = main;
+  const { icon } = weather[0];
+  let degreeCelcius = temp - 273;
+  let newTemp = parseFloat(degreeCelcius).toFixed(0);
+  const description = weather[0].main;
+  const country  = sys.country;
+  const weatherDiv = document.getElementById("weather");
+  weatherDiv.innerHTML = `
+    <div class="card">
+      <div class="card-header">
+      <div class="topRow">
+      <div id="icon"><img id="wicon" src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather icon"></div>
+      <p class="mb1 temperatureClass">${newTemp}°C</p>
+      <p class="mb1">${name}, ${country}</h2>
+      </div>
+      <p class="mb1">${description}</p>
+      <p class="mb1">Humidity: ${humidity}%</p>
+      </div>
+      </div>
+      `;
+}
+
+function styleWeatherCard(weatherId) {
+  $('#weather').removeClass("cloudyWeatherResults snowWeatherResults clearWeatherResults");
+  $('#weather').each(function() {
+    if (weatherId > 800 && weatherId < 805) {
+      $(this).addClass('cloudyWeatherResults');
+    } else if (weatherId > 199 && weatherId < 233) {
+      $(this).addClass('THUNDERSTORM');
+    } else if (weatherId > 299 && weatherId < 322) {
+      $(this).addClass('DRIZZLE');
+    } else if (weatherId > 499 && weatherId < 523) {
+      $(this).addClass('RAIN');
+    } else if (weatherId > 599 && weatherId < 623) {
+      $(this).addClass('snowWeatherResults');
+    } else if (weatherId > 700 && weatherId < 781) {
+      $(this).addClass('MIST/SMOKE/HAZE');
+    } else if (weatherId === 800) {
+      $(this).addClass('clearWeatherResults');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var input = document.getElementById("location");
+  input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const location = document.getElementById("location").value;
+      getWeatherData(location);
+    }
+  });
+
+  document.getElementById("searchButton").addEventListener("click", function() {
+    const location = document.getElementById("location").value;
+    getWeatherData(location);
+  });
 });
--- server.js
+++ server.js
@@ -8,86 +8,82 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   if (req.url.startsWith('/weather')) {
-    const cityName = req.url.split('/')[2];
-
-    if (!cityName) {
-      res.writeHead(400);
-      res.end('Invalid request! Please provide a city name.');
-      return;
-    }
-
-    const apiKey = config.apiKey;
-    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-    const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&cnt=5&appid=${apiKey}`;
-
-    // Fetch current weather data
-    https.get(weatherApiUrl, (weatherApiRes) => {
-      let weatherData = '';
-
-      weatherApiRes.on('data', (chunk) => {
-        weatherData += chunk;
-      });
-
-      weatherApiRes.on('end', () => {
-        // Fetch forecast data
-        https.get(forecastApiUrl, (forecastApiRes) => {
-          let forecastData = '';
-
-          forecastApiRes.on('data', (chunk) => {
-            forecastData += chunk;
-          });
-
-          forecastApiRes.on('end', () => {
-            const responseData = {
-              current: JSON.parse(weatherData),
-              forecast: JSON.parse(forecastData)
-            };
-
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(responseData));
-          });
-        }).on('error', (err) => {
-          res.writeHead(500);
-          res.end('Error fetching forecast data from OpenWeatherMap API: ' + err.message);
-        });
-      });
-    }).on('error', (err) => {
-      res.writeHead(500);
-      res.end('Error fetching weather data from OpenWeatherMap API: ' + err.message);
-    });
+    handleWeatherRequest(req, res);
   } else {
-    // Serve static files
-    let filePath = '.' + req.url;
-    if (filePath == './') {
-      filePath = './index.html';
-    }
-
-    const extname = String(path.extname(filePath)).toLowerCase();
-    const mimeTypes = {
-      '.html': 'text/html',
-      '.js': 'text/javascript',
-      '.css': 'text/css',
-      '.json': 'application/json',
-      '.png': 'image/png',
-      '.jpg': 'image/jpg',
-      '.svg': 'image/svg+xml'
-    };
-
-    const contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function (err, content) {
-      if (err) {
-        res.writeHead(500);
-        res.end('Error reading file: ' + err.code);
-      } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(content, 'utf-8');
-      }
-    });
+    serveStaticFile(req, res);
   }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function handleWeatherRequest(req, res) {
+  const cityName = req.url.split('/')[2];
+
+  if (!cityName) {
+    sendResponse(res, 400, 'Invalid request! Please provide a city name.');
+    return;
+  }
+
+  const apiKey = config.apiKey;
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  https.get(weatherApiUrl, (weatherApiRes) => {
+    let weatherData = '';
+
+    weatherApiRes.on('data', (chunk) => {
+      weatherData += chunk;
+    });
+
+    weatherApiRes.on('end', () => {
+      const responseData = JSON.parse(weatherData);
+      sendResponse(res, 200, responseData);
+    });
+  }).on('error', (err) => {
+    sendResponse(res, 500, 'Error fetching weather data from OpenWeatherMap API: ' + err.message);
+  });
+}
+
+function serveStaticFile(req, res) {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = path.extname(filePath).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg': 'image/svg+xml'
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      sendResponse(res, 500, 'Error reading file: ' + err.code);
+    } else {
+      sendResponse(res, 200, content, contentType);
+    }
+  });
+}
+
+function sendResponse(res, statusCode, data, contentType = 'application/json') {
+  res.writeHead(statusCode, { 'Content-Type': contentType });
+
+  if (typeof data === 'string') {
+    res.end(data, 'utf-8');
+  } else if (Buffer.isBuffer(data)) {
+    res.end(data);
+  } else {
+    res.end(JSON.stringify(data));
+  }
+}
